a37997ccd242f4f42d491c4cb925ce054026c38e
gibert-j <adelgibert@gmail.com>
2022-08-03 22:31:51 +1200

    const for fm. ensuring resouce path. let for reading/storing contents

--- ImageViewer/ViewController.swift
+++ ImageViewer/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // assign the value from file manager.default to fm
+        let fm = FileManager.default
+        // searches the directory/bundle containing the compiled app/assets
+        // ! necessary since some non-iOS bundles may lack resouce path. This forces resource dir
+        let path = Bundle.main.resourcePath!
+        // try to read and store in array contents of above path in items const
+        // try! will crash app if items not found. However contents MUST be made to be there
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        }
     }
-
-
 }
-
