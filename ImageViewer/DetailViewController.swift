50820f685fa7b75af28a27c9d38f81c2c0433b1b
gibert-j <adelgibert@gmail.com>
2022-08-05 01:30:45 +1200

    Added bar button to share Added share method

--- ImageViewer/DetailViewController.swift
+++ ImageViewer/DetailViewController.swift
@@ -19,9 +19,12 @@ class DetailViewController: UIViewController {
         
         title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
+        
         navigationController!.navigationBar.backgroundColor = .white
         
-        
+        // creates new right bar button item with parameters
+        // .action is a system icon. target and action parameters tell the UIBarButtonItem what method to call (shareTapped)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         
         // checks optional in selectedImage. If selectedImage is null, code won't execute
         if let imageToLoad = selectedImage {
@@ -39,6 +42,20 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    // @objc tells swift to compile this method for swift & visible to objective-c code (like UIBarButtonItem)
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        // share image using UIActivitiyViewController
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        // show ready UIActivityViewController in screen
+        present(vc, animated: true)
     }
     
     /*
